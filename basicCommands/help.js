a162eb56867083362079672cc9071d91ba2600fb
MRH7780 <166525065+MRH7780@users.noreply.github.com>
2024-04-09 20:01:48 +0530

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -1,6 +1,6 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
-const { ButtonStyle } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const db = require("../mongodb");
+
 module.exports = {
   name: 'help',
   aliases: ['hlp', 'h'],
@@ -9,14 +9,14 @@ module.exports = {
     const botUser = message.client.user;
     const botPing = Date.now() - message.createdTimestamp;
     const serverCount = message.client.guilds.cache.size;
+    
     const embed = new EmbedBuilder()
       .setColor('#2b71ec')
       .setAuthor({
-        name: 'Im here to Help!',
+        name: 'I\'m here to Help!',
         iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
         url: 'https://discord.gg/FUEHs7RCqz'
-    })
-     
+      })
       .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **TEST  #005 MODE:-music**__ `)
       .addFields(
         // Basic commands category
@@ -31,19 +31,19 @@ module.exports = {
           value: '`play`, `stop`, `history`,`volume`,`pause`,`resume`,`247`',
           inline: true,
         },
-        //fun category
+        // Fun category
         {
           name: '▶️  Fun',
           value: ' `ascii`, `joke`, `meme`, `roll`',
           inline: true,
         },
-        //image category
+        // Image category
         {
           name: '▶️  Image',
           value: '`cat`, `dog`',
           inline: true,
         },
-        //anime category
+        // Anime category
         {
           name: '▶️  Anime',
           value: '`<prefix>animecommands for more info`',
@@ -58,7 +58,7 @@ module.exports = {
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
       .setImage(`https://media.discordapp.net/attachments/1225773994423091230/1227263258940145755/250a5d2d0ffe5968a1f44a3372c9b14c.png?ex=6627c4fa&is=66154ffa&hm=917e0dbc94728263b05013ba4369622f56311616455a98cf77ab15916c6982f0&=&format=webp&quality=lossless&width=160&height=160`);
-    
-    message.reply({ embeds: [embed], components: [row] });
+
+    message.reply({ embeds: [embed] });
   },
 };
